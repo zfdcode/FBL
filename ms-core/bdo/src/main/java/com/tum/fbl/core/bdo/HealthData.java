69799fd8e793b15f5f15e1b073276ccd8444aef4
Malte Brockmann <ga46siz@mytum.de>
2017-05-30 12:01:03 +0200

    Added User model

--- ms-core/bdo/src/main/java/com/tum/fbl/core/bdo/HealthData.java
+++ ms-core/bdo/src/main/java/com/tum/fbl/core/bdo/HealthData.java
@@ -4,4 +4,5 @@ package com.tum.fbl.core.bdo;
  * Created by patrickmelchner on 30.05.17.
  */
 public class HealthData {
+
 }
--- ms-core/bdo/src/main/java/com/tum/fbl/core/bdo/User.java
+++ ms-core/bdo/src/main/java/com/tum/fbl/core/bdo/User.java
@@ -1,7 +1,87 @@
 package com.tum.fbl.core.bdo;
+import java.util.Date;
 
-/**
- * Created by patrickmelchner on 30.05.17.
- */
+
 public class User {
+
+    private int user_id;
+
+    private String user_name;
+
+    private String user_password;
+
+    private String email;
+
+    private Date birthday;
+
+    private int height;
+
+    private int weight;
+
+    public User(int user_id, String user_name, String user_password, String email, Date birthday, int height, int weight) {
+        this.user_id = user_id;
+        this.user_name = user_name;
+        this.user_password = user_password;
+        this.email = email;
+        this.birthday = birthday;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getUser_password() {
+        return user_password;
+    }
+
+    public void setUser_password(String user_password) {
+        this.user_password = user_password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
 }

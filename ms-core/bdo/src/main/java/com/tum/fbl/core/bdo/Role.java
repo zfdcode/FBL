5ef79bdd1038bdfdc4a4aa660ac77f347b64a569
zfdcode <zfdstudy@outlook.com>
2017-06-04 20:12:11 +0200

    Add Role class

---
+++ ms-core/bdo/src/main/java/com/tum/fbl/core/bdo/Role.java
@@ -0,0 +1,32 @@
+package com.tum.fbl.core.bdo;
+
+/**
+ * Created by zfngd on 2017/6/4.
+ */
+public class Role {
+
+    private int role_id;
+
+    private String role_name;
+
+    public Role(int role_id, String role_name) {
+        this.role_id = role_id;
+        this.role_name = role_name;
+    }
+
+    public int getRole_id() {
+        return role_id;
+    }
+
+    public void setRole_id(int role_id) {
+        this.role_id = role_id;
+    }
+
+    public String getRole_name() {
+        return role_name;
+    }
+
+    public void setRole_name(String role_name) {
+        this.role_name = role_name;
+    }
+}

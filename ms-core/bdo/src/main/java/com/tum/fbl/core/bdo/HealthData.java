717211faf1b9acb5bb79a39edaa4676ad934e9ae
Malte Brockann <ga46siz@mytum.de>
2017-06-08 18:21:15 +0200

    Added bdo for health Data, orders, restaurant & wearable

--- ms-core/bdo/src/main/java/com/tum/fbl/core/bdo/HealthData.java
+++ ms-core/bdo/src/main/java/com/tum/fbl/core/bdo/HealthData.java
@@ -5,4 +5,94 @@ package com.tum.fbl.core.bdo;
  */
 public class HealthData {
 
+    private User user;
+
+    private String device_user_password;
+
+    private String device_user_id;
+
+    private int current_hearthbeat;
+
+    private int heartbeat_in_a_time_range;
+
+    private float tracked_calorie_today;
+
+    private float tracked_calories_in_a_time_range;
+
+    private int average_heartbeat_in_a_time_range;
+
+    public HealthData(User user, String device_user_password, String device_user_id, int current_hearthbeat, int heartbeat_in_a_time_range, float tracked_calorie_today, float tracked_calories_in_a_time_range, int average_heartbeat_in_a_time_range) {
+        this.user = user;
+        this.device_user_password = device_user_password;
+        this.device_user_id = device_user_id;
+        this.current_hearthbeat = current_hearthbeat;
+        this.heartbeat_in_a_time_range = heartbeat_in_a_time_range;
+        this.tracked_calorie_today = tracked_calorie_today;
+        this.tracked_calories_in_a_time_range = tracked_calories_in_a_time_range;
+        this.average_heartbeat_in_a_time_range = average_heartbeat_in_a_time_range;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getDevice_user_password() {
+        return device_user_password;
+    }
+
+    public void setDevice_user_password(String device_user_password) {
+        this.device_user_password = device_user_password;
+    }
+
+    public String getDevice_user_id() {
+        return device_user_id;
+    }
+
+    public void setDevice_user_id(String device_user_id) {
+        this.device_user_id = device_user_id;
+    }
+
+    public int getCurrent_hearthbeat() {
+        return current_hearthbeat;
+    }
+
+    public void setCurrent_hearthbeat(int current_hearthbeat) {
+        this.current_hearthbeat = current_hearthbeat;
+    }
+
+    public int getHeartbeat_in_a_time_range() {
+        return heartbeat_in_a_time_range;
+    }
+
+    public void setHeartbeat_in_a_time_range(int heartbeat_in_a_time_range) {
+        this.heartbeat_in_a_time_range = heartbeat_in_a_time_range;
+    }
+
+    public float getTracked_calorie_today() {
+        return tracked_calorie_today;
+    }
+
+    public void setTracked_calorie_today(float tracked_calorie_today) {
+        this.tracked_calorie_today = tracked_calorie_today;
+    }
+
+    public float getTracked_calories_in_a_time_range() {
+        return tracked_calories_in_a_time_range;
+    }
+
+    public void setTracked_calories_in_a_time_range(float tracked_calories_in_a_time_range) {
+        this.tracked_calories_in_a_time_range = tracked_calories_in_a_time_range;
+    }
+
+    public int getAverage_heartbeat_in_a_time_range() {
+        return average_heartbeat_in_a_time_range;
+    }
+
+    public void setAverage_heartbeat_in_a_time_range(int average_heartbeat_in_a_time_range) {
+        this.average_heartbeat_in_a_time_range = average_heartbeat_in_a_time_range;
+    }
 }
--- ms-core/bdo/src/main/java/com/tum/fbl/core/bdo/Restaurant.java
+++ ms-core/bdo/src/main/java/com/tum/fbl/core/bdo/Restaurant.java
@@ -4,4 +4,150 @@ package com.tum.fbl.core.bdo;
  * Created by patrickmelchner on 30.05.17.
  */
 public class Restaurant {
+
+    private int restaurant_id;
+
+    private String restaurant_password;
+
+    private String restaurant_name;
+
+    private String restaurant_email;
+
+    private String restaurant_display_name;
+
+    private String restaurant_address;
+
+    private Date[] restaurant_opening_hour_monday;
+
+    private Date[] restaurant_opening_hour_tuesday;
+
+    private Date[] restaurant_opening_hour_wednesday;
+
+    private Date[] restaurant_opening_hour_thursday;
+
+    private Date[] restaurant_opening_hour_friday;
+
+    private Date[] restaurant_opening_hour_saturday;
+
+    private Date[] restaurant_opening_hour_sunday;
+
+    public Restaurant(int restaurant_id, String restaurant_password, String restaurant_name, String restaurant_email, String restaurant_display_name, String restaurant_address, Date[] restaurant_opening_hour_monday, Date[] restaurant_opening_hour_tuesday, Date[] restaurant_opening_hour_wednesday, Date[] restaurant_opening_hour_thursday, Date[] restaurant_opening_hour_friday, Date[] restaurant_opening_hour_saturday, Date[] restaurant_opening_hour_sunday) {
+        this.restaurant_id = restaurant_id;
+        this.restaurant_password = restaurant_password;
+        this.restaurant_name = restaurant_name;
+        this.restaurant_email = restaurant_email;
+        this.restaurant_display_name = restaurant_display_name;
+        this.restaurant_address = restaurant_address;
+        this.restaurant_opening_hour_monday = restaurant_opening_hour_monday;
+        this.restaurant_opening_hour_tuesday = restaurant_opening_hour_tuesday;
+        this.restaurant_opening_hour_wednesday = restaurant_opening_hour_wednesday;
+        this.restaurant_opening_hour_thursday = restaurant_opening_hour_thursday;
+        this.restaurant_opening_hour_friday = restaurant_opening_hour_friday;
+        this.restaurant_opening_hour_saturday = restaurant_opening_hour_saturday;
+        this.restaurant_opening_hour_sunday = restaurant_opening_hour_sunday;
+    }
+
+    public int getRestaurant_id() {
+        return restaurant_id;
+    }
+
+    public void setRestaurant_id(int restaurant_id) {
+        this.restaurant_id = restaurant_id;
+    }
+
+    public String getRestaurant_password() {
+        return restaurant_password;
+    }
+
+    public void setRestaurant_password(String restaurant_password) {
+        this.restaurant_password = restaurant_password;
+    }
+
+    public String getRestaurant_name() {
+        return restaurant_name;
+    }
+
+    public void setRestaurant_name(String restaurant_name) {
+        this.restaurant_name = restaurant_name;
+    }
+
+    public String getRestaurant_email() {
+        return restaurant_email;
+    }
+
+    public void setRestaurant_email(String restaurant_email) {
+        this.restaurant_email = restaurant_email;
+    }
+
+    public String getRestaurant_display_name() {
+        return restaurant_display_name;
+    }
+
+    public void setRestaurant_display_name(String restaurant_display_name) {
+        this.restaurant_display_name = restaurant_display_name;
+    }
+
+    public String getRestaurant_address() {
+        return restaurant_address;
+    }
+
+    public void setRestaurant_address(String restaurant_address) {
+        this.restaurant_address = restaurant_address;
+    }
+
+    public Date[] getRestaurant_opening_hour_monday() {
+        return restaurant_opening_hour_monday;
+    }
+
+    public void setRestaurant_opening_hour_monday(Date[] restaurant_opening_hour_monday) {
+        this.restaurant_opening_hour_monday = restaurant_opening_hour_monday;
+    }
+
+    public Date[] getRestaurant_opening_hour_tuesday() {
+        return restaurant_opening_hour_tuesday;
+    }
+
+    public void setRestaurant_opening_hour_tuesday(Date[] restaurant_opening_hour_tuesday) {
+        this.restaurant_opening_hour_tuesday = restaurant_opening_hour_tuesday;
+    }
+
+    public Date[] getRestaurant_opening_hour_wednesday() {
+        return restaurant_opening_hour_wednesday;
+    }
+
+    public void setRestaurant_opening_hour_wednesday(Date[] restaurant_opening_hour_wednesday) {
+        this.restaurant_opening_hour_wednesday = restaurant_opening_hour_wednesday;
+    }
+
+    public Date[] getRestaurant_opening_hour_thursday() {
+        return restaurant_opening_hour_thursday;
+    }
+
+    public void setRestaurant_opening_hour_thursday(Date[] restaurant_opening_hour_thursday) {
+        this.restaurant_opening_hour_thursday = restaurant_opening_hour_thursday;
+    }
+
+    public Date[] getRestaurant_opening_hour_friday() {
+        return restaurant_opening_hour_friday;
+    }
+
+    public void setRestaurant_opening_hour_friday(Date[] restaurant_opening_hour_friday) {
+        this.restaurant_opening_hour_friday = restaurant_opening_hour_friday;
+    }
+
+    public Date[] getRestaurant_opening_hour_saturday() {
+        return restaurant_opening_hour_saturday;
+    }
+
+    public void setRestaurant_opening_hour_saturday(Date[] restaurant_opening_hour_saturday) {
+        this.restaurant_opening_hour_saturday = restaurant_opening_hour_saturday;
+    }
+
+    public Date[] getRestaurant_opening_hour_sunday() {
+        return restaurant_opening_hour_sunday;
+    }
+
+    public void setRestaurant_opening_hour_sunday(Date[] restaurant_opening_hour_sunday) {
+        this.restaurant_opening_hour_sunday = restaurant_opening_hour_sunday;
+    }
 }

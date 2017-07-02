49e0f55401cebe42d8464c2fa1d7d2700a7589f4
Patrick Melchner <patrick.melchner@gmail.com>
2017-07-02 15:45:07 +0200

    Refactored image upload API

--- ms-core/service/src/main/java/com/tum/fbl/core/service/resources/MealResource.java
+++ ms-core/service/src/main/java/com/tum/fbl/core/service/resources/MealResource.java
@@ -8,6 +8,7 @@ import com.tum.fbl.core.imagestorage.ImageStorageImpl;
 import com.tum.fbl.core.bdo.Meal;
 import com.tum.fbl.core.persistence.ConnectionFactory;
 import com.tum.fbl.core.service.auth.User;
+import com.tum.fbl.core.service.exceptions.ImageException;
 import io.dropwizard.auth.Auth;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -80,16 +81,15 @@ public class MealResource {
     @Path("/img")
     @POST
     @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public Response uploadFile(
+    public ImageStatus uploadFile(
             @FormDataParam("file") InputStream uploadedInputStream,
             @FormDataParam("file") FormDataContentDisposition fileDetail
     ){
         try {
-            final ImageStatus imageStatus = this.imageStorage.saveImage(uploadedInputStream, fileDetail.getFileName());
-            return Response.ok().build();
+            return this.imageStorage.saveImage(uploadedInputStream, fileDetail.getFileName());
         } catch (ImagePersistenceException e) {
             LOGGER.error("Image Upload failed");
-            return  Response.serverError().build();
+            throw new ImageException("File could not be uploaded");
         }
     }
 

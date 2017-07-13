cd6768ea90828b30f263cb48468259e421787ee1
angusliu <angus.liu@tum.de>
2017-07-13 14:55:07 +0200

    Updates Fitbit Class

--- ms-core/fitbit/src/main/java/com/tum/fbl/core/fitbit/test.java
+++ ms-core/fitbit/src/main/java/com/tum/fbl/core/fitbit/test.java
@@ -5,7 +5,8 @@ package com.tum.fbl.core.fitbit;
  */
 public class test {
     public static void main(String[]args){
-        FitBitImpl Value1=new FitBitImpl("eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1UTlZSE4iLCJhdWQiOiIyMjhGNkwiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJwcm8gcm51dCByc2xlIiwiZXhwIjoxNDk5MjkwMjc3LCJpYXQiOjE0OTkyNjE0Nzd9.uEb2PRGW58qxmjMx1gTqaG9YACPt5fo-Y6BHK9aj_D4");
-        System.out.print(Value1.apiCall("https://api.fitbit.com/1/user/-/activities/goals/caloriesOut/daily.json"));
+        FitBitImpl Value1=new FitBitImpl("eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1UTlZSE4iLCJhdWQiOiIyMjhGNkwiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJudXQgcnBybyByc2xlIiwiZXhwIjoxNDk5OTYwMzcwLCJpYXQiOjE0OTk5MzE1NzB9.mkq6lly75HcQf4yN5o6LzLEmFn4Mr8wuUpVUI1oVW1c");
+        System.out.print(Value1.burnedCalories());
+        System.out.print(Value1.calorieGoal());
     }
 }
